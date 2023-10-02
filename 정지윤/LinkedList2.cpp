062ccc01914d5bab87852bb848852a7c79053baf
jiyoon77 <jungjiyo@naver.com>
2023-10-03 04:19:55 +0900

    코드는 다 짰는데 값을 입력할때 문제가 발생합니다

---
+++ 정지윤/LinkedList2.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct Student {
+	char* name;
+	int age;
+	int id;
+} typedef Student;
+
+struct LinkedList {
+	Student* data;
+	LinkedList* next;
+} typedef LinkedList;
+
+LinkedList* addData(LinkedList* LinkedList1, Student* students) {
+	LinkedList* newLinkedList = (LinkedList*)malloc(sizeof(LinkedList));
+
+	if (newLinkedList == NULL) {
+		printf("실패\n");
+	}
+	newLinkedList->data = students;
+	newLinkedList->next = LinkedList1;
+	return newLinkedList;
+
+}
+
+void printData(LinkedList* LinkedList1) {
+	LinkedList* first = LinkedList1;
+	while (first != NULL) {
+		Student* students = first->data;
+		printf("이름: %s, 나이: %d, 학번: %d\n", students->name, students->age, students->id);
+		first = first->next;
+	}
+}
+int main() {
+	int menu = 0;
+	LinkedList* LinkedList1 = NULL;
+	int data;
+	struct Student students;
+
+	while (menu!= 3) {
+
+		printf("메뉴: 1.삽입 2.전체 조회 3.종료\n");
+		printf("메뉴를 고르셈.\n");
+		scanf("%d", &menu);
+
+		if (menu == 1) {
+			Student* students = (Student*)malloc(sizeof(Student));
+
+			printf("학생의 이름과 나이, 학번을 입력하세요.\n");
+			scanf("%s %d %d", students->name, &students->age, &students->id);
+
+			LinkedList1=addData(LinkedList1, students);
+		}
+		else if (menu == 2) {
+			printData(LinkedList1);
+		}
+	}
+	if (menu == 3) {
+		printf("종료됨.\n");
+	}
+	
+	while (LinkedList1 != NULL) {
+		LinkedList* temp = LinkedList1;
+		LinkedList1 = LinkedList1->next;
+		free(temp);
+	}
+
+	return 0;
+}
