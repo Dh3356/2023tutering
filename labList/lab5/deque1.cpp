be94ec188d4d5ca923f3325ce6403c3b5049e24f
Kim Daehyeon <daehyeon3351@gmail.com>
2023-11-02 17:58:20 +0900

    lab5 출제

---
+++ labList/lab5/deque1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	//한 문자열을 입력받아 해당 문자열이 앞으로 읽어도 뒤로 읽어도 같은 문자열인지 판별,
+	//true 또는 false를 출력하는 프로그램을 작성.
+	//dequq, stack 등을 사용할 때는 라이브러리를 include 하여 사용해라.
+	//입력 예시: helleh
+	//출력 예시: true
+	//입력 예시: hello
+	//출력 예시: false
+	return 0;
+}
